01760573eaed293a2393540084dafe490a666779
Matthew Madden <matthewkayin@gmail.com>
2023-02-04 08:43:34 -0800

    commit for playtest
    Signed-off-by: Matthew Madden <matthewkayin@gmail.com>

--- player/player.gd
+++ player/player.gd
@@ -1,12 +1,18 @@
 extends KinematicBody2D
 
 onready var hook_timer = $hook_timer
+onready var jump_timer = $jump_timer
+onready var coyote_timer = $coyote_timer
+onready var delay_gravity_timer = $delay_gravity_timer
 onready var raycast = $raycast
 onready var hairline = $hairline
+onready var sprite = $sprite
+onready var hair_sprite = $hair_sprite
 
+const DECELERATION = 24
 const VELOCITY = 128
 const MAX_VELOCITY = 64 * 4
-const GRAVITY = 16
+const GRAVITY = 24
 const MAX_JUMP_HEIGHT = 16 * 3
 const MAX_HOOK_DIST = 640
 
@@ -14,12 +20,17 @@ const HOOK_DELAY_TIME = 0.3
 const HOOK_SPEED = 32
 const HOOK_PULL_SPEED = 256 * 3
 
+const JUMP_INPUT_DURATION = 0.06
+const COYOTE_TIME_DURATION = 0.06
+
 var velocity = Vector2.ZERO
 var direction = Vector2.ZERO
 var facing_direction = 1
+var grounded = false
 
 var jump_height = null
 var nearest_hook = null
+var hair_length = 0
 
 enum HookState {
     NONE,
@@ -34,6 +45,11 @@ func _ready():
     hook_timer.connect("timeout", self, "_on_hook_timer_timeout")
 
 func _physics_process(_delta):
+    if Input.is_action_just_pressed("up"):
+        hair_length = min(hair_length + 1, 4)
+    if Input.is_action_just_pressed("down"):
+        hair_length = max(hair_length - 1, 0)
+
     # Direction input
     if Input.is_action_pressed("right"):
         direction.x = 1
@@ -44,7 +60,9 @@ func _physics_process(_delta):
     else:
         direction.x = 0
 
-    # Jump release input
+    # Jump input
+    if Input.is_action_just_pressed("jump"):
+        jump_timer.start(JUMP_INPUT_DURATION)
     if jump_height != null and (position.y <= jump_height or Input.is_action_just_released("jump")):
         jump_height = null
 
@@ -69,19 +87,39 @@ func _physics_process(_delta):
     if hook_state == HookState.PULL:
         velocity = position.direction_to(nearest_hook.position) * HOOK_PULL_SPEED
     else:
-        velocity.x = direction.x * VELOCITY
+        if not grounded:
+            print(velocity)
+        if abs(velocity.x) > MAX_VELOCITY or direction.x == 0:
+            var decel_direction = -1
+            if velocity.x < 0:
+                decel_direction = 1
+            var target_velocity = MAX_VELOCITY * direction.x
+
+            var deceleration = DECELERATION
+            if not grounded:
+                deceleration *= 0.5
+
+            if abs(velocity.x - target_velocity) <= deceleration:
+                velocity.x = target_velocity
+            else:
+                velocity.x += deceleration * decel_direction
+        else:
+            velocity.x = direction.x * VELOCITY
         if jump_height == null:
             velocity.y += GRAVITY
         else:
             velocity.y = -MAX_VELOCITY
-        velocity.y = clamp(velocity.y, -MAX_VELOCITY, MAX_VELOCITY)
+        #velocity.y = clamp(velocity.y, -MAX_VELOCITY, MAX_VELOCITY)
 
-    var grounded = is_on_floor()
+    var was_grounded = grounded
+    grounded = is_on_floor()
+    if was_grounded and not grounded:
+        coyote_timer.start(COYOTE_TIME_DURATION)
     if grounded and velocity.y >= 5:
         velocity.y = 5
 
     # Jump input
-    if grounded and Input.is_action_pressed("jump") and hook_state == HookState.NONE:
+    if (grounded or not coyote_timer.is_stopped()) and not jump_timer.is_stopped() and hook_state == HookState.NONE:
         jump_height = position.y - MAX_JUMP_HEIGHT
 
     # Movement
@@ -94,6 +132,8 @@ func _physics_process(_delta):
     # Hook Search
     if hook_state == HookState.NONE:
         search_hooks()
+
+    update_sprite()
 
 func search_hooks():
     nearest_hook = null
@@ -124,3 +164,26 @@ func search_hooks():
 func _on_hook_timer_timeout():
     hook_state = HookState.PULL
     jump_height = null
+
+func update_sprite():
+    if grounded and velocity.x == 0:
+        sprite.play("idle")
+    elif grounded:
+        sprite.play("run")
+    elif not grounded and velocity.y == 0:
+        sprite.play("jump_apex")
+    elif not grounded and velocity.y > 0:
+        sprite.play("jump_fall")
+    elif not grounded and jump_height != null and abs(position.y - jump_height) > 16:
+        sprite.play("jump")
+    elif not grounded: 
+        sprite.play("jump_rise")
+
+    if hair_length == 0:
+        hair_sprite.play("0")
+    else:
+        hair_sprite.animation = String(hair_length) + "_" + sprite.animation
+        hair_sprite.frame = sprite.frame
+    
+    sprite.flip_h = facing_direction == -1
+    hair_sprite.flip_h = sprite.flip_h
